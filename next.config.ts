e4d06aae8a565592000148568df0cd02cc7bc5f0
Salath Joseph <salathjoseph@Salaths-MacBook-Air.local>
2026-07-27 22:35:07 +0530

    Configure root basePath for custom domain hosting

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/wahs_crm' : '',
 };
 
 export default nextConfig;
